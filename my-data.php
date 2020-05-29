1720df128a6988de79bf4f016402e2d6a383821d
david <david.chandra@10up.com>
2020-05-29 12:58:42 +0700

    Formatting.

--- my-data.php
+++ my-data.php
@@ -4,25 +4,25 @@
  */
 
 add_action( 'init', function() {
-register_post_meta(
-	'post',
-	'_my_data',
-	[
-		'show_in_rest' => true,
-		'single'       => true,
-		'type'         => 'string',
-		'auth_callback' => function() {
-			return current_user_can( 'edit_posts' );
-		}
-	]
-);
-add_action( 'enqueue_block_editor_assets', function() {
-	wp_enqueue_script(
-		'my-data',
-		trailingslashit( plugin_dir_url( __FILE__ ) ) . 'my-data.min.js',
-		[ 'wp-element', 'wp-blocks', 'wp-components', 'wp-editor' ],
-		'0.1.0',
-		true
+	register_post_meta(
+		'post',
+		'_my_data',
+		[
+			'show_in_rest' => true,
+			'single'       => true,
+			'type'         => 'string',
+			'auth_callback' => function() {
+				return current_user_can( 'edit_posts' );
+			}
+		]
 	);
-} );
+	add_action( 'enqueue_block_editor_assets', function() {
+		wp_enqueue_script(
+			'my-data',
+			trailingslashit( plugin_dir_url( __FILE__ ) ) . 'my-data.min.js',
+			[ 'wp-element', 'wp-blocks', 'wp-components', 'wp-editor' ],
+			'0.1.0',
+			true
+		);
+	} );
 } );
